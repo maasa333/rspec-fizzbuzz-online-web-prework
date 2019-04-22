282ddadefce381320b86dfe1f8243ab28df25942
Maasa Kono <konomaasa@gmail.com>
2019-04-22 00:10:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-
+require 'pry'
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
@@ -8,5 +8,6 @@ def fizzbuzz(int)
     "Buzz"
   else int % (3 && 5) == 0
     "FizzBuzz"
+    binding.pry
   end
 end
