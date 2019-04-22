ab3a15bfa8b54507dd39a1c61c405560989477e9
Maasa Kono <konomaasa@gmail.com>
2019-04-22 00:24:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,9 +6,9 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 || 5 == 0
+  elsif int % (3 && 5) == 0
     "FizzBuzz"
-  else int % 3 || 5 != 0
+  else int % (3 || 5) != 0
     "nil"
   end
 end
